900d6fcd018b50fb7fbd978fc9c4299f0abcbd54
Developer Email Address <dg978t@att.com>
2018-04-02 12:46:32 -0500

    Revised smartcab

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -39,7 +39,11 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Update epsilon using a decay function of your choice
-        self.epsilon = np.exp(-(1-self.alpha)*self.trials)
+        if(self.learning):
+            self.epsilon = self.epsilon * .9
+            # self.epsilon = self.epsilon - .05
+        else:
+            self.epsilon = self.epsilon - .05
         # self.epsilon = self.epsilon - 0.05
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
@@ -184,7 +188,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, epsilon = 1.0, alpha = .7)
+    agent = env.create_agent(LearningAgent, epsilon = 1.0, learning = True, alpha = .6)
     
     ##############
     # Follow the driving agent
@@ -199,14 +203,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay = 0.01, log_metrics = True, optimized = True)
+    sim = Simulator(env, update_delay = 0, log_metrics = True, optimized = True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(tolerance = 0.005, n_test = 10)
+    sim.run(tolerance = 0.005, n_test = 20)
 
 
 if __name__ == '__main__':
